b9625bac823e5cd45d37b5babe2cbb1969f54221
19terickson <traceerickson@mail.weber.edu>
2021-04-02 19:14:06 -0600

    First commit

---
+++ src/javascripts/movies.js
@@ -0,0 +1,85 @@
+export const movies = [{
+    "id": 1,
+    "title": "Pallette Bonfire",
+    "description": "By: Rafael Feroli",
+    "poster": "https://unsplash.com/photos/pt4ofgV8N04/download?force=true&w=640",
+  
+  },
+  {
+    "id": 2,
+    "title": "White Hot",
+    "description": "By: Patrick Hendry",
+    "poster": "https://unsplash.com/photos/6G9RMWqcOEA/download?force=true&w=640",
+
+  },
+  {
+    "id": 3,
+    "title": "Blue Phoenix",
+"description": "By: Sumit Suharkar",
+    "poster": "https://unsplash.com/photos/y2LtPzz2tds/download?force=true&w=640",
+
+  },
+  {
+    "id": 4,
+    "title": "Farenheit 451",
+    "description": "By: Gaspar Uhas",
+    "poster": "https://unsplash.com/photos/J7osQDpHvBI/download?force=true&w=640",
+ 
+  },
+  {
+    "id": 5,
+    "title": "A Natural Furnace",
+    "description": "By: James Bryant",
+    "poster": "https://unsplash.com/photos/3NGMSa2CSkw/download?force=true&w=640",
+  },
+  {
+    "id": 6,
+    "title": "Red Fire Tunnel",
+    "description": "By: Casey Horner",
+    "poster": "https://unsplash.com/photos/HkGUSayvq5U/download?force=true&w=640",
+ 
+  },
+  {
+    "id": 7,
+    "title": "The Smallest Lantern",
+    "description": "By: Abdullah Ahmad",
+    "poster": "https://unsplash.com/photos/IPmZFauMzlo/download?force=true&w=640",
+
+  },
+  {
+    "id": 8,
+    "title": "The Beauty of Energy",
+    "description": "By: Roy Clement",
+    "poster": "https://unsplash.com/photos/h9pP0xAbfKI/download?force=true&w=640",
+
+  },
+  {
+    "id": 9,
+    "title": "Incineration",
+    "description": "By: Christopher Burns",
+    "poster": "https://unsplash.com/photos/gyrrWzwqm5Y/download?force=true",
+   
+  },
+  {
+    "id": 10,
+    "title": "Lifelight",
+    "description": "By: Sadiq Nafee",
+    "poster": "https://unsplash.com/photos/_6_aEY3mKNs/download?force=true&w=640",
+
+  },
+  {
+    "id": 11,
+    "title": "Flames of the Night",
+    "description": "By Joshua Newton",
+    "poster": "https://unsplash.com/photos/8z9pi6GgOo4/download?force=true&w=640",
+ 
+},
+{
+"id": 12,
+"title": "Sparks Fly",
+"description": "By: Monil Andharia",
+"poster": "https://unsplash.com/photos/0xVrOF1oaPU/download?force=true&w=640",
+
+}
+
+]
